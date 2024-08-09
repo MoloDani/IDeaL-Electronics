964ae93422f0576a0f2ef855eece682e0f0740a1
MoloDani <126819876+MoloDani@users.noreply.github.com>
2024-08-09 15:29:45 +0300

    main

--- frontend/src/Containers/HeroSection.tsx
+++ frontend/src/Containers/HeroSection.tsx
@@ -1,7 +1,19 @@
 import { useState } from "react";
+import "./hero.css";
 
 function HeroSection() {
-  return <div className="hero-section"></div>;
+  return (
+    <div className="hero-section">
+      <div id="startingText">
+        <h3>FTC ROBOTICS TEAM</h3>
+        <h1 id="fade">IDeaL Electronics</h1>
+        <h2>RO032</h2>
+      </div>
+      <div className="image" id="teamPhoto">
+        <img src="\images\download.jpeg" alt="team photo" />
+      </div>
+    </div>
+  );
 }
 
 export default HeroSection;
