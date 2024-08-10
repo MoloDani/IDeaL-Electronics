5fab5a360f45491960042e3d6a255b343fe12f7b
MoloDani <126819876+MoloDani@users.noreply.github.com>
2024-08-10 15:00:15 +0300

    hero - updates

--- frontend/src/Containers/HeroSection.tsx
+++ frontend/src/Containers/HeroSection.tsx
@@ -6,12 +6,16 @@ function HeroSection() {
     <div className="hero-section">
       <div id="startingText">
         <h1 className="subHeading">FTC ROBOTICS TEAM</h1>
-        <h1 id="fade">IDeaL Electronics</h1>
+        <h1 id="textFade">IDeaL Electronics</h1>
         <h2>RO032</h2>
       </div>
 
       <div id="teamPhoto">
         <img className="image" src="\images\download.jpg" alt="team photo" />
+      </div>
+
+      <div className="bottomLine">
+        <hr></hr>
       </div>
     </div>
   );
