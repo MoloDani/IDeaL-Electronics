0c2d9ebf3adb8680150d6df05b5e7c5b14d9fdc6
MoloDani <126819876+MoloDani@users.noreply.github.com>
2024-08-11 14:23:20 +0300

    mobile-friendly

--- frontend/src/Containers/HeroSection.tsx
+++ frontend/src/Containers/HeroSection.tsx
@@ -3,14 +3,14 @@ import "./hero.css";
 
 function HeroSection() {
   return (
-    <div className="hero-section">
-      <div>
+    <div className="container" id="hero">
+      <div id="startText">
         <h3>FTC ROBOTICS TEAM</h3>
         <h1>IDeaL Electronics</h1>
         <h2>RO032</h2>
       </div>
 
-      <div>
+      <div className="imagine">
         <img src="\images\download.jpg" alt="team photo" />
       </div>
     </div>
