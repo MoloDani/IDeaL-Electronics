5bd1ff3f53690284e890b420e3028462e3a8b10b
MoloDani <126819876+MoloDani@users.noreply.github.com>
2024-08-19 18:27:19 +0300

    updated files

--- frontend/src/Containers/HeroSection.tsx
+++ frontend/src/Containers/HeroSection.tsx
@@ -6,9 +6,16 @@ function HeroSection() {
     <div className="container" id="hero">
       <div id="startText">
         <h3>FTC ROBOTICS TEAM</h3>
-        <h1 id="textFade">IDeaL Electronics</h1>
+        <h1>IDeaL Electronics</h1>
         <h2>RO032</h2>
       </div>
+<<<<<<< HEAD
+=======
+
+      <div className="imagine">
+        <img src="\images\download.jpg" alt="team photo" />
+      </div>
+>>>>>>> parent of dbb6680 (hero - almost finished)
     </div>
   );
 }
