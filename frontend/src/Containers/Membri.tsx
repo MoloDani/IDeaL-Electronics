7a7db9c1ad3091959fdc2e00d671301392ed3078
MoloDani <126819876+MoloDani@users.noreply.github.com>
2024-08-21 01:03:20 +0300

    am imbunatatit cardul de membrii care poate acum fi folosit si la mentori

--- frontend/src/Containers/Membri.tsx
+++ frontend/src/Containers/Membri.tsx
@@ -3,8 +3,11 @@ import "./membri.css";
 import Membru from "./Membru";
 
 export interface Member {
+  poza: string;
   nume: string;
   functie: string;
+  tip: string;
+  id?: string;
 }
 
 function Membri() {
@@ -22,10 +25,33 @@ function Membri() {
         <h2>Sa ne cunoastem mai bine!</h2>
       </div>
 
-      <div id="containerMemb">
-        {membri.map((mem: Member) => (
-          <Membru nume={mem.nume} functie={mem.functie} key={mem.nume} />
-        ))}
+      <div className="containerMemb">
+        <Membru
+          key={1}
+          poza="./images/download.jpg"
+          nume="Georgi"
+          functie="PR"
+          tip="side"
+        />
+
+        <Membru
+          key={2}
+          poza="./images/membrii/daniel.jpg"
+          nume="Daniel"
+          functie="programare"
+          tip="center"
+        />
+
+        <Membru
+          key={3}
+          poza="./images/download.jpg"
+          nume="Tudor"
+          functie="programare"
+          tip="side"
+        />
+
+        <button className="slideButton" id="left"></button>
+        <button className="slideButton" id="right"></button>
       </div>
     </div>
   );
