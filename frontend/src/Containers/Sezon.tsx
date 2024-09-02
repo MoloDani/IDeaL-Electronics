7ea7d74b5b32acfc5f033eb441edb8f8a8272640
Alexandru Marinescu <127241987+AlexMarinescu123@users.noreply.github.com>
2024-09-02 11:18:39 +0300

    Changed image type + added scroll-down animation for seasons

--- frontend/src/Containers/Sezon.tsx
+++ frontend/src/Containers/Sezon.tsx
@@ -1,9 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { sez } from "./Sezoane";
 import "./sezon.css";
-import firstPlaceIcon from "../../images/ImagesBase64/firstPlace";
-import secondPlaceIcon from "../../images/ImagesBase64/secondPlace";
-import thirdPlaceIcon from "../../images/ImagesBase64/thirdPlace";
 
 
 // Asta e un card pentru un singur sezon
@@ -11,19 +8,19 @@ import thirdPlaceIcon from "../../images/ImagesBase64/thirdPlace";
 function Sezon({ name, img, awardType1, rewardPlace1, competitionStage1, 
   awardType2, rewardPlace2, competitionStage2 }: sez) {
   return (
-    <div className="sezCard">
+    <div className="sezCard sezon-card">
       <div className="logoSez">
       <img src={"data:image/jpeg;base64, " + img} alt={"logo sezon: " + name} />
       <h3>{name}</h3>
       </div>
       <div className= "award">
-        <img src={"data:image/jpeg;base64, " + firstPlaceIcon} alt="award image"></img>
+        <img src={".\\images\\" + rewardPlace1 + ".png"} alt="award image"></img>
         <h3>{awardType1}</h3>
         <h3>{competitionStage1}</h3>
       </div>
 
       <div className= "award">
-        <img src={"data:image/jpeg;base64, " + secondPlaceIcon} alt="award image"></img>
+        <img src={".\\images\\" + rewardPlace2 + ".png"} alt="award image"></img>
         <h3>{awardType2}</h3>
         <h3>{competitionStage2}</h3>
       </div>
