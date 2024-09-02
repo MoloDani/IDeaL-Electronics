5ff7551c019a3fff88e00dbd256de3e2ea2637c6
MoloDani <126819876+MoloDani@users.noreply.github.com>
2024-09-02 20:23:00 +0300

    backend created

--- frontend/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default {
-  server: {
-    port: 3000,  // Change 3000 to your desired port
-  },
-};
